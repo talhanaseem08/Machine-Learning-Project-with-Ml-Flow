0b10bf3885eaf1e854f28827eb7216f56cdce384
talhanaseem08 <talhan094@gmail.com>
2025-08-06 00:19:18 +0500

    create directories & download data

---
+++ src/mlProject/constants/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
